09dffebf1731600cd748c0d2766eb689a3b14f40
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-02-13 16:56:02 -0300

    Add: Geckolib as dependency

--- src/main/java/com/syllient/livingchest/LivingChest.java
+++ src/main/java/com/syllient/livingchest/LivingChest.java
@@ -8,11 +8,13 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import software.bernie.geckolib3.GeckoLib;
 
-@Mod(modid = LivingChest.MOD_ID, name = LivingChest.NAME, version = LivingChest.VERSION)
+@Mod(modid = LivingChest.MOD_ID, name = LivingChest.NAME, version = LivingChest.VERSION,
+    dependencies = LivingChest.DEPENDENCIES)
 public class LivingChest implements Proxy {
   public static final String MOD_ID = "livingchest";
   public static final String NAME = "LivingChest";
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "1.0.0";
+  public static final String DEPENDENCIES = "required-after:geckolib3@[3.0.20,);";
 
   @Mod.Instance(LivingChest.MOD_ID)
   public static LivingChest INSTANCE;
