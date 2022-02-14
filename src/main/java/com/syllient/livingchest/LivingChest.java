0ff3366d124320dc546669fe841d52c56d46ab15
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-02-14 18:17:49 -0300

    Version

--- src/main/java/com/syllient/livingchest/LivingChest.java
+++ src/main/java/com/syllient/livingchest/LivingChest.java
@@ -13,7 +13,7 @@ import software.bernie.geckolib3.GeckoLib;
 public class LivingChest implements Proxy {
   public static final String MOD_ID = "livingchest";
   public static final String NAME = "LivingChest";
-  public static final String VERSION = "1.0.0.0";
+  public static final String VERSION = "1.0.0.1";
   public static final String DEPENDENCIES = "required-after:geckolib3@[3.0.20,);";
 
   @Mod.Instance(LivingChest.MOD_ID)
