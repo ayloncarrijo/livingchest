30a2a10c68453eb9227529bd223c7a5dd107cf17
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-02-16 20:09:44 -0300

    Test update.json

--- src/main/java/com/syllient/livingchest/client/renderer/EyeBoneTileEntityRenderer.java
+++ src/main/java/com/syllient/livingchest/client/renderer/EyeBoneTileEntityRenderer.java
@@ -2,10 +2,17 @@ package com.syllient.livingchest.client.renderer;
 
 import com.syllient.livingchest.client.model.EyeBoneTileEntityModel;
 import com.syllient.livingchest.tileentity.EyeBoneTileEntity;
+import net.minecraft.client.renderer.GlStateManager;
 import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
 
 public class EyeBoneTileEntityRenderer extends GeoBlockRenderer<EyeBoneTileEntity> {
   public EyeBoneTileEntityRenderer() {
     super(new EyeBoneTileEntityModel());
+  }
+
+  @Override
+  public void renderEarly(final EyeBoneTileEntity animatable, final float ticks, final float red,
+      final float green, final float blue, final float partialTicks) {
+    GlStateManager.scale(5f, 5f, 5f);
   }
 }
