abbd2f1c7c05709a7b3f5f8d034fe937a8ba0260
Aylon Carrijo <aylon.gabriel@outlook.com>
2022-02-19 21:01:51 -0300

    Feat: Eye Bone particles, shape and loot table

--- src/main/java/com/syllient/livingchest/block/EyeBoneBlock.java
+++ src/main/java/com/syllient/livingchest/block/EyeBoneBlock.java
@@ -11,11 +11,35 @@ import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraftforge.common.ToolType;
 
 public class EyeBoneBlock extends HorizontalBlock {
+  private static class Shape {
+    private static final VoxelShape EYE =
+        VoxelShapes.box(0.345D, 0.535D, 0.345D, 0.655D, 0.84D, 0.655D);
+
+    private static final VoxelShape GRIP =
+        VoxelShapes.box(0.45D, 0.1D, 0.45D, 0.55D, 0.535D, 0.55D);
+
+    private static final VoxelShape BASE_NS =
+        VoxelShapes.box(0.39D, 0.0D, 0.45D, 0.61D, 0.1D, 0.55D);
+
+    private static final VoxelShape BASE_WE =
+        VoxelShapes.box(0.45D, 0.0D, 0.39D, 0.55D, 0.1D, 0.61D);
+
+    private static final VoxelShape FULL_NS = VoxelShapes.or(EYE, GRIP, BASE_NS);
+
+    private static final VoxelShape FULL_WE = VoxelShapes.or(EYE, GRIP, BASE_WE);
+  }
+
   public EyeBoneBlock() {
-    super(AbstractBlock.Properties.of(Material.STONE));
+    super(
+        AbstractBlock.Properties.of(Material.STONE).harvestLevel(0).harvestTool(ToolType.PICKAXE));
     this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
   }
 
@@ -37,6 +61,15 @@ public class EyeBoneBlock extends HorizontalBlock {
   @Override
   public TileEntity createTileEntity(final BlockState state, final IBlockReader world) {
     return new EyeBoneTile();
+  }
+
+  @Override
+  public VoxelShape getShape(final BlockState state, final IBlockReader reader, final BlockPos pos,
+      final ISelectionContext ctx) {
+    final Direction direction = state.getValue(FACING);
+
+    return direction == Direction.WEST || direction == Direction.EAST ? Shape.FULL_WE
+        : Shape.FULL_NS;
   }
 
   @Override
