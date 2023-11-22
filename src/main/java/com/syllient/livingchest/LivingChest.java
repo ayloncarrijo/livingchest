97cd1bd0158360ace9ee89338a0666d7d597d947
Aylon Carrijo <aylon.gabriel@outlook.com>
2023-11-22 19:06:36 -0300

    chore: add initial project setup

---
+++ src/main/java/com/syllient/livingchest/LivingChest.java
@@ -0,0 +1,8 @@
+package com.syllient.livingchest;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LivingChest.MOD_ID)
+public class LivingChest {
+  public static final String MOD_ID = "livingchest";
+}
